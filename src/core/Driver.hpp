5098b44dbb24f776db1b3b2243c17a07c72b48f9
Sebastien Valat <sebastien.valat@atos.net>
2020-02-26 18:27:58 +0100

    [core] Start to define classes

---
+++ src/core/Driver.hpp
@@ -0,0 +1,24 @@
+/*****************************************************
+			 PROJECT  : ummap-io-v2
+			 LICENSE  : Apache 2.0
+			 COPYRIGHT: 2020 Bull SAS
+*****************************************************/
+
+#ifndef UMMAP_DRIVER_HPP
+#define UMMAP_DRIVER_HPP
+
+/********************  NAMESPACE  *******************/
+namespace ummap
+{
+
+/*********************  CLASS  **********************/
+class Driver
+{
+	public:
+		Driver(void);
+		virtual ~Driver(void);
+};
+
+}
+
+#endif //UMMAP_DRIVER_HPP
--- src/core/Mapping.hpp
+++ src/core/Mapping.hpp
@@ -1,5 +1,47 @@
 /*****************************************************
-			 PROJECT  : cdeps
+			 PROJECT  : ummap-io-v2
 			 LICENSE  : Apache 2.0
 			 COPYRIGHT: 2020 Bull SAS
 *****************************************************/
+
+#ifndef UMMAP_MAPPING_HPP
+#define UMMAP_MAPPING_HPP
+
+/********************  HEADERS  *********************/
+//std
+#include <cstdlib>
+//internal
+#include "Driver.hpp"
+
+/********************  NAMESPACE  *******************/
+namespace ummap
+{
+
+/*********************  STRUCT  *********************/
+struct SegmentStatus
+{
+	size_t flushTime:56;
+	unsigned int unused:6;
+	bool readAccess:1;
+	bool writeAccess:1;
+};
+
+/*********************  CLASS  **********************/
+class Mapping
+{
+	public:
+		Mapping(Driver * driver, size_t size, size_t segmentSize);
+		virtual ~Mapping(void);
+		void onSegmentationFault(void * address, bool isWrite);
+		void flush(void);
+		void flush(size_t offset, size_t size);
+		void prefetch(size_t offset, size_t size);
+	private:
+		Driver * driver;
+		void * baseAddress;
+		SegmentStatus * status;
+};
+
+}
+
+#endif //UMMAP_MAPPING_HPP
