f84865094629eb1f7d7765ca2487bf5fc3ff1945
Sebastien Valat <sebastien.valat@atos.net>
2020-02-26 19:35:34 +0100

    [core] Implement Driver

---
+++ src/core/Driver.cpp
@@ -0,0 +1,21 @@
+/*****************************************************
+			 PROJECT  : ummap-io-v2
+			 LICENSE  : Apache 2.0
+			 COPYRIGHT: 2020 Bull SAS
+*****************************************************/
+
+/********************  HEADERS  *********************/
+#include "Driver.hpp"
+
+/***************** USING NAMESPACE ******************/
+using namespace ummap;
+
+/*******************  FUNCTION  *********************/
+Driver::Driver(void)
+{
+}
+
+/*******************  FUNCTION  *********************/
+Driver::~Driver(void)
+{
+}
