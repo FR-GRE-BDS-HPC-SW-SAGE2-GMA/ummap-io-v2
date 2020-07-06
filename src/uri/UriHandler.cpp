523e70b66b0b11f46c2a536a7391e87ab18040e7
Sebastien Valat <sebastien.valat@atos.net>
2020-07-06 17:58:15 +0200

    [uri] Autocreate mero objects

--- src/uri/UriHandler.cpp
+++ src/uri/UriHandler.cpp
@@ -225,6 +225,7 @@ Driver * UriHandler::buildDriverMero(const Uri & uri)
 	//build driver
 	#ifdef MERO_FOUND
 		m0_uint128 m0id = {id.high, id.low};
+		create_object(m0id);
 		return new ClovisDriver(m0id);
 	#else
 		if (uri.getType() == "merofile")
