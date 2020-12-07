48740fa87138c027e573ecb9ee0009022002e54f
Sebastien Valat <sebastien.valat@atos.net>
2020-12-07 20:06:09 +0100

    [style] Convert codes in CRLF to LF

--- src/drivers/ClovisDriver.hpp
+++ src/drivers/ClovisDriver.hpp
@@ -1,6 +1,7 @@
 /*****************************************************
 			 PROJECT  : ummap-io-v2
 			 LICENSE  : Apache 2.0
+			 COPYRIGHT: 2020 Bull SAS
 *****************************************************/
 
 #ifndef UMMAP_CLOVIS_DRIVER_HPP
