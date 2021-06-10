233b414a4d6db9c7ac097d4d6780c5057356c262
Sebastien Valat <sebastien.valat@atos.net>
2021-06-10 15:21:01 +0200

    [motr] Update examples to run on mero or motr

--- bench/mero_examples/big_ios/big_ios.c
+++ bench/mero_examples/big_ios/big_ios.c
@@ -40,7 +40,14 @@ int main(int argc, char **argv)
         printf("Using Default Object ID=%lu:%lu\n", id.u_hi, id.u_lo);
     }
 
-    int ret = c0appz_init(0, ressource_file);
+    #if define(HAVE_MERO)
+        int ret = c0appz_init(0, ressource_file);
+    #elif defined(HAVE_MOTR)
+        c0appz_set_manual_rc(ressource_file);
+        int ret = c0appz_init(0);
+    #else
+        #error "Should choose either mero or motr !"
+    #endif
     if (ret == 0) {
        printf("[Success] Mero Initialized\n");
     } else {
@@ -50,12 +57,12 @@ int main(int argc, char **argv)
     ret = create_object(id);
     // Call ummap
     size_t size = 1000*1024*1024; //1GB
-    size_t segsize = 100*1024*1024; //16MB
+    size_t segsize = 100*1024*1024; //100MB
     off_t offset = 0;
     void *baseptr = NULL;
 
     ummap_init();
-    baseptr = ummap(NULL, size, segsize, offset, PROT_READ|PROT_WRITE, 0, ummap_driver_create_clovis(id.u_hi, id.u_lo, false) , NULL, "none");
+    baseptr = ummap(NULL, size, segsize, offset, PROT_READ|PROT_WRITE, UMMAP_NO_FIRST_READ, ummap_driver_create_clovis(id.u_hi, id.u_lo, false) , NULL, "none");
     char *index = (char *)baseptr;
 
     // Write the whole object with 1GB of data in it
