ac3b505dfb348e9415f87d2487d12cc7a2e86fc4
Sebastien Valat <sebastien.valat@atos.net>
2021-10-28 18:15:30 +0200

    [quota] Make implementation of inter-process quota, now need to test & debug

---
+++ src/core/PolicyQuotaInterProc.cpp
@@ -0,0 +1,172 @@
+/*****************************************************
+			 PROJECT  : ummap-io-v2
+			 VERSION  : 0.0.0-dev
+			 DATE     : 03/2020
+			 LICENSE  : ????????
+*****************************************************/
+
+/********************  HEADERS  *********************/
+//std
+#include <cassert>
+//unix
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <signal.h>
+//internal
+#include "../common/Debug.hpp"
+//local
+#include "Policy.hpp"
+#include "PolicyQuotaInterProc.hpp"
+
+/***************** USING NAMESPACE ******************/
+using namespace ummapio;
+
+/********************  MACROS  **********************/
+#define SHM_FFLAGS  (O_CREAT | O_RDWR)
+#define SHM_FPERM   (S_IRUSR | S_IWUSR)
+
+/*******************  FUNCTION  *********************/
+PolicyQuotaInterProc::PolicyQuotaInterProc(const std::string & name,size_t staticMaxMemory)
+                     :PolicyQuotaLocal(staticMaxMemory)
+{
+	//keep track
+	this->name = std::string("/") + name;
+	this->totalAllowed = staticMaxMemory;
+
+	//open
+	this->shared = static_cast<PolicyQuotaInterProcShared*>(this->openShm(this->name, sizeof(*this->shared)));
+
+	//CRITICAL SECTION
+	{
+		//take lock
+		this->lock();
+
+		//increment process number
+		this->shared->processes++;
+
+		//search a position in the list
+		bool found = false;
+		for (int i = 0 ; i < this->shared->indexMax ; ++i) {
+			if (this->shared->pids[i] == 0 || kill(this->shared->pids[i],0) == 0) {
+				this->shared->pids[i] = getpid();
+				found = true;
+				break;
+			}
+		}
+
+		//if not found add
+		if (found == false)
+			this->shared->pids[++(this->shared->indexMax)] = getpid();
+
+		//end of critical section
+		this->unlock();
+	}
+
+	//signal all
+	this->signalAll();
+
+	//update
+	this->update();
+}
+
+/*******************  FUNCTION  *********************/
+void PolicyQuotaInterProc::signalAll(void)
+{
+	//CRITICAL SECTION
+	{
+		//take lock
+		this->lock();
+
+		//loop on all to send signal
+		for (int i = 0 ; i < this->shared->indexMax ; i++) {
+			//check if still alive
+			if (this->shared->pids[i] != 0 && kill(this->shared->pids[i],0) == 0) {
+				this->shared->processes--;
+				this->shared->pids[i] = 0;
+			} else if (this->shared->pids[i] == getpid()) {
+				//nothing to do, not want to send to self
+			} else {
+				//send signal
+				sigval_t sigval = { .sival_ptr = NULL };
+				sigqueue(this->shared->pids[i], SIGEVICT, sigval);
+			}
+		}
+
+		//end of critical section
+		this->unlock();
+	}
+}
+
+
+/*******************  FUNCTION  *********************/
+void PolicyQuotaInterProc::lock(void)
+{
+	int expected = 0;
+	while(!this->shared->spinlock.compare_exchange_strong(expected,1)) {
+		//spin until can take the lock
+		expected = 0;
+	}
+}
+
+
+/*******************  FUNCTION  *********************/
+void PolicyQuotaInterProc::unlock()
+{
+	int expected = 1;
+	bool status = this->shared->spinlock.compare_exchange_strong(expected,0);
+	assume(status, "Fail to unlock, got invalid value on the spinlock !");
+}
+
+/*******************  FUNCTION  *********************/
+void PolicyQuotaInterProc::update(void)
+{
+	//calc quota
+	size_t perProcQuota = this->totalAllowed / this->shared->processes;
+	this->staticMaxMemory = perProcQuota;
+
+	//debug
+	IOC_DEBUG_ARG("quota:interproc", "Change quota for %1 computed from %2 processes").argUnit1024(perProcQuota).arg(this->shared->processes).end();
+
+	//call local update
+	PolicyQuotaLocal::update();
+}
+
+/*******************  FUNCTION  *********************/
+void * PolicyQuotaInterProc::openShm(const std::string &name, size_t size)
+{
+	//vars
+	int32_t fd = -1;
+	struct stat  st = { 0 };
+
+	//check
+	assert(size % 4096 == 0);
+
+	//open
+	fd = shm_open(name.c_str(), SHM_FFLAGS, SHM_FPERM);
+	assumeArg(fd >= 0, "Failed to shm_open('%1'): %2").arg(name).argStrErrno().end();
+
+	//stats
+	int status = fstat(fd, &st);
+	assumeArg(status == 0, "Failed to fstat the shm file '%1': %2").arg(name).argStrErrno().end();
+
+	//truncate
+	if (size > 0 && st.st_size == 0)
+	{
+		status = ftruncate(fd, size);
+		assumeArg(status == 0, "Failed to ftruncate the shm file '%1': %2").arg(name).argStrErrno().end();
+	}
+
+	//map
+	void * ptr = mmap(NULL, size, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, 0);
+	assumeArg(ptr != MAP_FAILED, "Failed to mmap('%1'): %2").arg(name).argStrErrno().end();
+
+	//close
+	status = close(fd);
+	assumeArg(status == 0, "Failed to close the shm file '%1': %2").arg(name).argStrErrno().end();
+
+	//ret
+	return ptr;
+}
---
+++ src/core/PolicyQuotaInterProc.hpp
@@ -0,0 +1,62 @@
+/*****************************************************
+			 PROJECT  : ummap-io-v2
+			 VERSION  : 0.0.0-dev
+			 DATE     : 02/2020
+			 LICENSE  : ????????
+*****************************************************/
+
+#ifndef UMMAP_POLICY_QUOTA_INTER_PROCESS_HPP
+#define UMMAP_POLICY_QUOTA_INTER_PROCESS_HPP
+
+/********************  HEADERS  *********************/
+//std
+#include <cstdlib>
+#include <list>
+#include <string>
+#include <mutex>
+#include <atomic>
+//internal
+#include "PolicyQuota.hpp"
+#include "PolicyQuotaLocal.hpp"
+
+/********************  NAMESPACE  *******************/
+namespace ummapio
+{
+
+/*********************  STRUCT  *********************/
+#define SHARED_TAB_ENTRIES ((1024 * 1024 - 4 * sizeof(std::atomic<int>)) / sizeof(pid_t))
+#define SIGEVICT SIGUSR1
+struct PolicyQuotaInterProcShared
+{
+	std::atomic<int> padding;
+	std::atomic<int> spinlock;
+	std::atomic<int> processes;
+	std::atomic<int> indexMax;
+	pid_t pids[SHARED_TAB_ENTRIES];
+};
+
+/*********************  STRUCT  *********************/
+/**
+ * Implement the quota sharing between policies between processes.
+ * The sharing is made with a simple rule, each process get 1/Nth of
+ * the total allowed memory.
+**/
+class PolicyQuotaInterProc : public PolicyQuotaLocal
+{
+	public:
+		PolicyQuotaInterProc(const std::string & name,size_t staticMaxMemory);
+		virtual void update(void) override;
+	private:
+		void * openShm(const std::string &name, size_t size);
+		void signalAll(void);
+		void lock(void);
+		void unlock(void);
+	private:
+		std::string name;
+		PolicyQuotaInterProcShared * shared;
+		size_t totalAllowed;
+};
+
+}
+
+#endif //UMMAP_POLICY_QUOTA_INTER_PROCESS_HPP
