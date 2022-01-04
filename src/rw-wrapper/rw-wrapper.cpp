bdeacbd69de277ed6d5169800cf0d5bf2ef57a69
Sebastien Valat <sebastien.valat@atos.net>
2022-01-04 12:17:13 +0100

    [rw-wrapper] Implement read support and directly call mapping segfault handler instead of making real fault.

--- src/rw-wrapper/rw-wrapper.cpp
+++ src/rw-wrapper/rw-wrapper.cpp
@@ -34,7 +34,7 @@ char libWriteGblStore[UMMAP_STORE_SIZE];
  * Signature of the system write operation to be able to use the
  * function extracted by dlsym().
 **/
-typedef ssize_t (*ummap_libc_write_t)(int fd, const void *buf, size_t count);
+typedef ssize_t (*ummap_libc_rw_t)(int fd, const void *buf, size_t count);
 
 /*******************  FUNCTION  *********************/
 //export as C interface
@@ -46,23 +46,22 @@ extern "C" {
 /**
  * Make a read first touch access on the segment to force ummap to map it.
 **/
-static void ummap_read_touch(const void * buf, size_t size)
+static void ummap_touch(ummapio::Mapping & mapping, const void * buf, size_t size, bool isWrite)
 {
 	//special
 	if (size == 0)
 		return;
 
 	//cast
-	const char * cast_buffer = (const char *)buf;
+	char * cast_buffer = (char *)buf;
 
 	//touch one element per page
 	size_t i;
 	for (i = 0 ; i < size ; i += 4096)
-		libWriteGblStore[i % UMMAP_STORE_SIZE] = cast_buffer[i];
+		mapping.onSegmentationFault(cast_buffer+i, isWrite);
 	
 	//touch last
-	i = size - 1;
-	libWriteGblStore[i % UMMAP_STORE_SIZE] = cast_buffer[i];
+	mapping.onSegmentationFault(cast_buffer + size - 1, isWrite);
 }
 
 /*******************  FUNCTION  *********************/
@@ -72,9 +71,9 @@ ssize_t write(int fd, const void *buf, size_t count)
 	void * buffer = (void*)buf;
 
 	//extract the original write symbol
-	static ummap_libc_write_t libc_write = NULL;
+	static ummap_libc_rw_t libc_write = NULL;
 	if (libc_write == NULL) {
-		libc_write = (ummap_libc_write_t)dlsym(RTLD_NEXT, "write");
+		libc_write = (ummap_libc_rw_t)dlsym(RTLD_NEXT, "write");
 		assert(libc_write != NULL);
 	}
 
@@ -97,10 +96,53 @@ ssize_t write(int fd, const void *buf, size_t count)
 		size_t fetch = std::min(count, policy_max);
 		
 		//prefetch by reading one element per page
-		ummap_read_touch(buffer, fetch);
+		ummap_touch(*mapping, buffer, fetch, false);
 
 		//call again write
 		res = libc_write(fd, buffer, count);
+
+		//debug
+		//fprintf(stderr, "============> CAPTURED %p => %zu => %zd <============\n", buf, count, res);
+	}
+	return res;
+}
+
+/*******************  FUNCTION  *********************/
+ssize_t read(int fd, const void *buf, size_t count)
+{
+	//cast for easer user
+	void * buffer = (void*)buf;
+
+	//extract the original read symbol
+	static ummap_libc_rw_t libc_read = NULL;
+	if (libc_read == NULL) {
+		libc_read = (ummap_libc_rw_t)dlsym(RTLD_NEXT, "read");
+		assert(libc_read != NULL);
+	}
+
+	//make a first try
+	ssize_t res = libc_read(fd, buffer, count);
+
+	//not ok => need to prefetch
+	if (res == -1) {
+		//get mapping and check it is backed to ummap-io
+		ummapio::Mapping * mapping = ummapio::getGlobalhandler()->getMapping(buffer);
+
+		//if not, it is not our problem, forward the error to the caller
+		if (mapping == NULL)
+			return -1;
+
+		//get the max memory from the mapping policies
+		size_t policy_max = mapping->getPolicyMaxMemory();
+
+		//calc what to prefetch
+		size_t fetch = std::min(count, policy_max);
+		
+		//prefetch by reading one element per page
+		ummap_touch(*mapping, buffer, fetch, false);
+
+		//call again read
+		res = libc_read(fd, buffer, count);
 
 		//debug
 		//fprintf(stderr, "============> CAPTURED %p => %zu => %zd <============\n", buf, count, res);
